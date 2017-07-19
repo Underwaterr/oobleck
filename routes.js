1a19cb3c6b1161b87562a4036d15d0633df6e8ce
Tyler Lane <tylerl@eurekasoft.com>
2017-07-19 15:45:08 -0500

    Show error nicely

--- routes.js
+++ routes.js
@@ -8,17 +8,23 @@ let inside  = 'http://localhost:3000/submissions'
 // Homepage GET
 router.get('/', (request, response) => {
     superagent
-    .get(inside)
-    .end(function(error, data) {
-        if (error) {
-            //response.render('error', {error: error })
-            response.json(error)
-        }
-        else {
-            let submissions = data.body
-            response.render('index', { submissions: submissions })            
-        }
-    })
+        .get(inside)
+        .end(function(error, data) {
+            if (error) {
+                response.render('error', parseError(error))
+            }
+            else {
+                let submissions = data.body
+                response.render('index', { submissions: submissions })            
+            }
+        })
 })
+
+let parseError = function(error) {
+    return {
+        status: error.status,
+        message: error.response.text
+    }
+}
 
 module.exports = router
