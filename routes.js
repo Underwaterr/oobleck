e72df07704ece301fd8dacff182c831fd6f4ab23
Tyler Lane <tylerl@eurekasoft.com>
2017-07-19 16:44:30 -0500

    Detect Heroku for api call

--- routes.js
+++ routes.js
@@ -2,13 +2,17 @@ let express = require('express')
 let router = express.Router()
 let superagent = require('superagent')
 
-let outside = 'http://oobleck-api.herokuapp.com/submissions'
-let inside  = 'http://localhost:3000/submissions'
+if (process.env.HEROKU) {
+    let uri = 'http://oobleck-api.herokuapp.com/submissions'
+}
+else {
+    let uri = 'http://localhost:3000/submissions'
+}
 
 // Homepage GET
 router.get('/', (request, response) => {
     superagent
-        .get(outside)
+        .get(uri)
         .end(function(error, data) {
             if (error) {
                 response.render('error', parseError(error))
