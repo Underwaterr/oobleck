03d5eda59b5e2cdcaf9f707e5f15ae0c2183d39d
Tyler Lane <sld.potato@gmail.com>
2017-07-27 17:08:57 -0500

    Added some GRAPHzzz

--- submissions/routes.js
+++ submissions/routes.js
@@ -12,7 +12,21 @@ router.get('/submissions', mustBeLoggedIn, mustBeRole(['admin', 'reviewer']), mu
         .set('Authorization', 'Bearer ' + request.access_token)
         .end(function(error, data) {
             let sortedSubmissions = data.body.sort((a, b)=> {return a.name.localeCompare(b.name)})
-            response.render('submissions', { submissions: sortedSubmissions})
+
+            let userId = response.locals.user.id
+            let scoreDistribution = [0, 0, 0, 0, 0, 0]
+            let numberOfSubmissionsUserReviewed = 0
+            sortedSubmissions.forEach(function(submission) {
+                if (submission.reviews[userId] !== undefined) {
+                    scoreDistribution[submission.reviews[userId].score]++
+                    numberOfSubmissionsUserReviewed++
+                }
+                else
+                    scoreDistribution[5]++
+            })
+            let totalNumberOfSubmissions = sortedSubmissions.length
+            let percentProgress = (numberOfSubmissionsUserReviewed / totalNumberOfSubmissions) * 100
+            response.render('submissions', { submissions: sortedSubmissions, scoreDistribution: scoreDistribution, percentProgress: percentProgress})
         })
 })
 
